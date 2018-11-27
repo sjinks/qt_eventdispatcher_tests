8c0607ebc526236500f31300f4e5a3fa8d933082
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2018-11-27 03:32:21 +0200

    Bug fix

--- tests-qt5/issues/libevent4/main.cpp
+++ tests-qt5/issues/libevent4/main.cpp
@@ -1,3 +1,4 @@
+#include <QtCore/QCoreApplication>
 #include <QtQuickTest/quicktest.h>
 #include "eventdispatcher.h"
 #include "eventdispatcherqpa.h"
@@ -6,7 +7,11 @@ int main(int argc, char** argv)
 {
 	EventDispatcher* d = new EventDispatcher();
 	QCoreApplication::setEventDispatcher(d);
+#ifdef QTEST_ADD_GPU_BLACKLIST_SUPPORT
 	QTEST_ADD_GPU_BLACKLIST_SUPPORT
+#endif
+#ifdef QTEST_SET_MAIN_SOURCE_PATH
 	QTEST_SET_MAIN_SOURCE_PATH
+#endif
 	return quick_test_main(argc, argv, "issue4", QUICK_TEST_SOURCE_DIR);
 }
